938e980a2cf7137f610ebce0d27ea81377f3c160
Paul Berry <paul.berry@prettylittlething.com>
2016-01-26 12:08:26 +0000

    test

--- stock.js
+++ stock.js
@@ -1,17 +1,14 @@
+var request = require('request');
+
 
 module.exports =
     function (context, req, res) {
         //get stock
-        var request = require('request');
         var textString = context.body.text;
         stock = textString.replace('stock: ','');
         var url = 'http://finance.yahoo.com/d/quotes.csv?s=';
         url = url+stock+"&f=nab";
         request.get(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var csv = body;
-                //res.setHeader('Content-Type', 'application/json');
-                res.send("hello");
-            }
+            res.send("hello");
         });
     }
